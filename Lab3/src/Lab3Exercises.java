61502db04a3b19c65524ad264f346d8591a1073e
4010a1 <4010a1@acerola.ece.uprm.edu>
2017-02-08 08:31:04 -0400

    Patch Notes: -Window size fixed -JFrame title changed -Pop up location adjusted

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,12 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		int heightMod, widthMod;
+		
+		JFrame myFrame = new JFrame("Fernando Agosto");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation((1920/2)-100 ,(1080/2)-100);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
